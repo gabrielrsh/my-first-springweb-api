4f3c067109210b897bf277a2d6c3ed405b46c0fc
gabrielrsh <gabriel.shiota@gmail.com>
2023-02-03 15:46:14 -0300

    cria projeto com spring initializr

---
+++ src/main/java/dio/myfirstspringwebapi/MyFirstSpringwebApiApplication.java
@@ -0,0 +1,13 @@
+package dio.myfirstspringwebapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyFirstSpringwebApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyFirstSpringwebApiApplication.class, args);
+	}
+
+}
