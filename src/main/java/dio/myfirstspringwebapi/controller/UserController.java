317658054bb5e05a776be090dbc03dee82d58692
gabrielrsh <gabriel.shiota@gmail.com>
2023-02-03 18:02:00 -0300

    adiciona rotas de delete,post e getbyname

--- src/main/java/dio/myfirstspringwebapi/controller/UserController.java
+++ src/main/java/dio/myfirstspringwebapi/controller/UserController.java
@@ -3,20 +3,41 @@ package dio.myfirstspringwebapi.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import dio.myfirstspringwebapi.model.User;
 import dio.myfirstspringwebapi.repository.UserRepository;
 
 @RestController
+@RequestMapping("/users")
 public class UserController {
 
     @Autowired
     UserRepository repository;
 
-    @GetMapping("/users")
+    @GetMapping
     public List<User> getUsers(){
         return repository.findAll();
+    }
+
+    @GetMapping("/{username}")
+    public User getOne(@PathVariable("username") String username){
+        return repository.findByUsername(username);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteUser(@PathVariable Integer id){
+        repository.deleteById(id);
+    }
+
+    @PostMapping
+    public void postUser(@RequestBody User user){
+        repository.save(user);
     }
 }
